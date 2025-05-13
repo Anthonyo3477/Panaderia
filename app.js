9499c8b31fd4169dd88d7bb7dd0e2182aa0c3d19
Wygers <vicente.bravo.romero@gmail.com>
2025-05-13 16:55:52 -0400

    avance vistas

--- app.js
+++ app.js
@@ -3,32 +3,20 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-// Establecer EJS como motor de plantillas
+// Configuración de EJS y vistas
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-// Rutas estáticas
-app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Middlewares
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-////////////////////////////////////////////////////////////////////////////
 
-// Rutas
-//const express = require('express');
-//const path = require('path');
-
-// Configurar motor de vistas
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// Servir archivos estáticos
+// Configuración adicional para archivos estáticos
 app.use('/css', express.static(path.join(__dirname, 'public/css')));
-////////////////////////////////////////////////////////////////////////////
-// Rutas
+
+// Rutas principales
 app.get('/', (req, res) => {
     res.render('Index', { title: 'Inicio' });
 });
@@ -40,7 +28,20 @@ app.get('/Panaderia', (req, res) => {
 app.get('/Reposteria', (req, res) => {
     res.render('Reposteria', { title: 'Repostería' });
 });
-//////////////////////////////////////////////////////////
+
+app.get('/Carro', (req, res) => {
+    res.render('Carro', { title: 'Carro de Compras' });
+});
+
+app.get('/Login_Registrar', (req, res) => {
+    res.render('Login_Registrar', { title: 'Iniciar Sesión / Registrarse' });
+});
+
+// Ruta para manejar errores 404
+app.use((req, res, next) => {
+    res.status(404).render('404', { title: 'Página no encontrada' });
+});
+
 // Iniciar servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
